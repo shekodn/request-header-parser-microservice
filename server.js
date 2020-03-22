a3a738d1c89a4a6523ca454dde1f1e84135a6eb2
shekodn <sdiazn19@gmail.com>
2020-03-22 12:54:47 -0600

    Add whoami endpoint

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,14 +19,27 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
 
+app.get("/api/whoami", function (req, res) {
+
+  const userAgent = req.get('user-agent')
+  const language  = req.get('accept-language')
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  res.json({
+    ipaddress: ip,
+    language: language,
+    software: userAgent
+  });
+});
+
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen("3000", function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
